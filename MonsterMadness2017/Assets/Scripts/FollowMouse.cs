66067da3e6b7210f5f41d5982fb238ae6c668500
DeeCeptor's Laptop <sirmichaellong@gmail.com>
2017-05-20 12:55:24 -0600

    no message

---
+++ MonsterMadness2017/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    public float z_position = -5f;
+
+
+	void Update ()
+    {
+		if (Time.timeScale != 0)
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = z_position;
+            this.transform.position = pos;
+        }
+    }
+}
