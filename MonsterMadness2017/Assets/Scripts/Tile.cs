6f5fe4c19a6b21f4e1e00a107ec87b4bbf5f2f18
Squirrell <ksquires11@gmail.com>
2017-05-20 10:53:41 -0600

    puff and level working on people now

--- MonsterMadness2017/Assets/Scripts/Tile.cs
+++ MonsterMadness2017/Assets/Scripts/Tile.cs
@@ -161,6 +161,8 @@ public class Tile : MonoBehaviour
         Vector3 cur_position = transform.position;
         transform.position = TileManager.tileManager.curTileSelected.transform.position;
         tile_to_swap.transform.position = cur_position;
+        Instantiate(Resources.Load("smoke_puff"),transform.position,transform.rotation);
+        Instantiate(Resources.Load("smoke_puff"), cur_position, transform.rotation);
         Find_Neighbours();
 
         //have all neighbours re-evaluate as well
