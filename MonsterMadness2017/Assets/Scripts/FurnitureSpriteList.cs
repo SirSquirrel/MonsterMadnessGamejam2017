2e1b45287162c74ce30492e4ed5369c1ad143f63
Mitchell <mitchellarthurcraig@gmail.com>
2017-05-20 15:00:00 -0600

    List of furniture added!

---
+++ MonsterMadness2017/Assets/Scripts/FurnitureSpriteList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureSpriteList : MonoBehaviour {
+    public Sprite[] furnitureSprites;
+    public static FurnitureSpriteList furnitureSpriteList;
+    // Use this for initialization
+    void Start()
+    {
+        furnitureSpriteList = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Sprite GetFurnitureSprite()
+    {
+        int rand = Random.Range(0, furnitureSprites.Length);
+        return furnitureSprites[rand];
+    }
+}
