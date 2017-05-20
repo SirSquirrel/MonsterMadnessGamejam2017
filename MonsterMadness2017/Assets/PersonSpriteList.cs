a377ec07236e1b7d0a7a054e956d1d0366b54574
Squirrell <ksquires11@gmail.com>
2017-05-20 12:41:05 -0600

    peoples

---
+++ MonsterMadness2017/Assets/PersonSpriteList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonSpriteList : MonoBehaviour {
+    public Sprite[] personSprites;
+    public static PersonSpriteList personSpriteList;
+	// Use this for initialization
+	void Start () {
+        personSpriteList = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Sprite GetPersonSprite()
+    {
+        int rand = Random.Range(0, personSprites.Length);
+        return personSprites[rand];
+    }
+}
