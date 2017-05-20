7b6357fa52669e71a17589743e11d762866fc5f4
DeeCeptor's Laptop <sirmichaellong@gmail.com>
2017-05-19 19:43:59 -0600

    no message

--- MonsterMadness2017/Assets/Scripts/Person.cs
+++ MonsterMadness2017/Assets/Scripts/Person.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class Person : MonoBehaviour
 {
+    public Vector2 walking_direction;
+    public float walking_speed = 1.0f;
+
     Vector2 destination;
     Vector2 prev_destination;
 
@@ -19,8 +22,9 @@ public class Person : MonoBehaviour
 
 	void Update ()
     {
-	    // Walk towards the destination
-        
+        // Walk towards the destination
+        this.transform.position = (Vector2) transform.position + walking_direction * walking_speed;
+
         // Are we at destination? If, figure out where we can turn
 
 	}
@@ -28,7 +32,11 @@ public class Person : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Exit")
+        if (collision.tag == "Tile")
+        {
+            cur_tile = collision.gameObject.GetComponent<Tile>();
+        }
+        else if (collision.tag == "Exit")
         {
             Debug.Log("Person has escaped the mansion!", this.gameObject);
             GameState.game_state.Defeat();
