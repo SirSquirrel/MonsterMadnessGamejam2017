c873b64298312a5588ddd4c56f7ca243f35cc6fa
Squirrell <ksquires11@gmail.com>
2017-05-19 20:09:37 -0600

    Tiles

---
+++ MonsterMadness2017/Assets/TileManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+    public static TileManager tileManager;
+    public Tile curTileSelected;
+    public bool hasTileSelected; 
+
+	// Use this for initialization
+	void Start () {
+        tileManager = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
