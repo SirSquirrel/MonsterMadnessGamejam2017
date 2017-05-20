41e54a75bff5d7bc75833cf750c7dab669cce389
Squirrell <ksquires11@gmail.com>
2017-05-19 21:16:32 -0600

    pre merge shennanigans

--- MonsterMadness2017/Assets/TileManager.cs
+++ MonsterMadness2017/Assets/TileManager.cs
@@ -6,7 +6,8 @@ public class TileManager : MonoBehaviour {
 
     public static TileManager tileManager;
     public Tile curTileSelected;
-    public bool hasTileSelected; 
+    public bool hasTileSelected;
+    public GameObject highlight;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +16,15 @@ public class TileManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetMouseButtonDown(1) && !TileManager.tileManager.hasTileSelected)
+        {
+            Debug.Log("Deselected");
+            TileManager.tileManager.hasTileSelected = false;
+            TileManager.tileManager.curTileSelected = null;
+            if (highlight!=null)
+            {
+                Destroy(highlight);
+            }
+        }
+    }
 }
