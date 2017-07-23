44ed42ba8793516c4c84c217be6a354fda3a4f74
DeeCeptor <sirmichaellong@gmail.com>
2017-07-23 16:43:50 -0600

    Added 2 new songs, changed selection/death animations, changed door opening sound effect

--- MonsterMadness2017/Assets/Scripts/MonsterCollision.cs
+++ MonsterMadness2017/Assets/Scripts/MonsterCollision.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterCollision : MonoBehaviour {
 
     public AudioSource scream;
-	// Use this for initialization
-	void Start () {
+
+    void Start ()
+    {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    void Update ()
+    {
 		
 	}
 
