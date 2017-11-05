f2c0b1551ff3981a2b2b9bab12b2a56077ca0831
DeeCeptor <sirmichaellong@gmail.com>
2017-11-05 01:39:11 -0600

    Fixed levels not loading in the build.

--- MonsterMadness2017/Assets/Scripts/LevelManager.cs
+++ MonsterMadness2017/Assets/Scripts/LevelManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Text.RegularExpressions;
 
 public static class LevelManager
 {
@@ -16,10 +16,53 @@ public static class LevelManager
     [RuntimeInitializeOnLoadMethod]
     public static void ReadLevelFile()
     {
-        string path = "Assets/Resources/LevelOrder.txt";
-        StreamReader file = new StreamReader(path);
+        List<string> current_level_list = novice_levels;
+        TextAsset file = Resources.Load("LevelOrder") as TextAsset;
+        Debug.Log(file);
+        string[] linesFromfile = Regex.Split(file.text, "\r\n"); //file.text.Split( "\n"[0] );
+        foreach(string line in linesFromfile)
+        {
+            //string line_temp = Regex.Replace(line, @"[^\u0009^\u000A^\u000D^\u0020-\u007E]", "");
+            string line_temp = line.Replace(System.Environment.NewLine, "");
+            switch (line_temp)
+            {
+                case "Novice":
+                    current_level_list = novice_levels;
+                    break;
+                case "Intermediate":
+                    current_level_list = intermediate_levels;
+                    break;
+                case "Expert":
+                    current_level_list = expert_levels;
+                    break;
+                default:
+                    // Not a new level category, add the level name to the current list
+                    current_level_list.Add(line);
+                    break;
+            }
+            /*
+            if (line.Contains("Novice"))
+            {
+                current_level_list = novice_levels;
+            }
+            else if (line.Contains("Intermediate"))
+            {
+                current_level_list = intermediate_levels;
+            }
+            else if (line.Contains("Expert"))
+            {
+                current_level_list = expert_levels;
+            }
+            else
+            {
+                current_level_list.Add(line);
+            }
+            */
+        }
+        //StreamReader file = new StreamReader(path);
 
         // Go line by line
+        /*
         string line;
         List<string> current_level_list = novice_levels;
         while ((line = file.ReadLine()) != null)
@@ -41,10 +84,9 @@ public static class LevelManager
                     break;
             }
         }
-
         file.Close();
-
-        Debug.Log("Finished reading level file file");
+        */
+        Debug.Log("Finished reading level file");
     }
 
 
